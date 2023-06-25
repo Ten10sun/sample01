d12be18bfea1f9e845af967b212d6aba47df0cc2
github TenTenSun <compacten10@gmail.com>
2023-06-25 16:55:23 +0900

    adding sample22.php

---
+++ sample21/submit.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>sample21
+  </title>
+</head>
+<body>
+  <h2>ご予約日</h2>
+  <?php $_REQUEST['reserve']; ?>
+  <ul>
+    <li>1/1</li>
+  </ul>
+  
+  <!-- <ul>
+    <?php foreach ($a as $b): ?>
+        <li><?php echo htmlspecialchars($b, ENT_QUOTES); ?></li>
+    <?php endforeach; ?>
+  </ul> -->
+
+</body>
+</html>
